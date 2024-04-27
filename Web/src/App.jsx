433e5d550aeeb7166b11397e8fc325be25afcf62
panjyar <pranjiyarkehuli@gmail.com>
2024-04-28 00:33:22 +0530

    card initialized

--- Web/src/App.jsx
+++ Web/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import {BrowserRouter as Router , Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignIn from "./Components/Login/SignIn.jsx";
 import SignUp from "./Components/Login/SignUp.jsx";
 import Home from "./Components/MainPage/Home.jsx";
@@ -10,13 +10,15 @@ function App() {
 
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/signin" element={<SignIn/>}></Route>
-          <Route path="/signup" element={<SignUp/>}></Route>
-          <Route path="/home" element={<Home/>}></Route>
-        </Routes>
-      </Router>
+      <div className="bg-gray-300">
+        <Router>
+          <Routes>
+            <Route path="/" element={<SignIn />}></Route>
+            <Route path="/signup" element={<SignUp />}></Route>
+            <Route path="/home" element={<Home />}></Route>
+          </Routes>
+        </Router>
+      </div>
     </>
   );
 }
--- Web/src/Components/MainPage/Home.jsx
+++ Web/src/Components/MainPage/Home.jsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="max-w-0.5">
+      <div className="relative flex flex-col text-gray-700 bg-red-400 shadow-md bg-clip-border rounded-3xl w-96">
+        <div className="relative mx-4 mt-4 overflow-hidden text-gray-700 bg-white shadow-lg bg-clip-border rounded-2xl h-80">
+          <img
+            src="https://docs.material-tailwind.com/img/team-3.jpg"
+            alt="profile-picture"
+          />
+        </div>
+        <div className="p-6 text-center">
+          <h4 className="block mb-2 font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+            Natalie Paisley
+          </h4>
+          <p className="block font-sans text-base antialiased font-medium leading-relaxed  bg-clip-text bg-gradient-to-tr from-blue-gray-600 to-blue-gray-400 ">
+            CEO / Co-Founder
+          </p>
+        </div>
+        <div className="flex justify-center p-6 pt-2 gap-7">
+          <a
+            href="#facebook"
+            className="block font-sans text-xl antialiased font-normal leading-relaxed text-transparent bg-clip-text bg-gradient-to-tr from-blue-600 to-blue-400"
+          >
+            <i className="fab fa-facebook" aria-hidden="true"></i>
+          </a>
+          <a
+            href="#twitter"
+            className="block font-sans text-xl antialiased font-normal leading-relaxed text-transparent bg-clip-text bg-gradient-to-tr from-light-blue-600 to-light-blue-400"
+          >
+            <i className="fab fa-twitter" aria-hidden="true"></i>
+          </a>
+          <a
+            href="#instagram"
+            className="block font-sans text-xl antialiased font-normal leading-relaxed text-transparent bg-clip-text bg-gradient-to-tr from-purple-600 to-purple-400"
+          >
+            <i className="fab fa-instagram" aria-hidden="true"></i>
+          </a>
+        </div>
+        <div className="flex justify-between p-6">
+          <button className="flex-grow bg-red-500 text-white px-4 py-2 rounded-md mr-4">
+            Love
+          </button>
+          <button className="flex-grow bg-blue-500 text-white px-4 py-2 rounded-md mr-4">
+            Message
+          </button>
+          <button className="flex-grow bg-green-500 text-white px-4 py-2 rounded-md">
+            Profile View
+          </button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
